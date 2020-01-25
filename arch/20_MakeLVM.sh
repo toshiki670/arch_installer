f38f2668fea8754962982831d1b70fa40e7c98a4
Toshiki <toshiki.dev@protonmail.ch>
2020-01-26 06:13:16 +0900

    Change

--- arch/20_MakeLVM.sh
+++ arch/20_MakeLVM.sh
@@ -17,8 +17,12 @@ cryptsetup -v -c serpent-xts-plain64 -s 512 -h sha512 luksFormat ${path}
 cryptsetup luksOpen ${path} decrypted
 pvcreate /dev/mapper/decrypted
 vgcreate system /dev/mapper/decrypted
-lvcreate -L 50G system -n root
-lvcreate -l 100%FREE system -n home
+
+lvcreate -l 100%FREE system -n root
+
+# For next version
+# lvcreate -L 50G system -n root
+# lvcreate -l 100%FREE system -n home
 
 mkfs.xfs /dev/mapper/system-root
 mkfs.xfs /dev/mapper/system-home
