c3a683465b63d0b40ee0b1c2fcffe5808cb32d13
Toshiki <toshiki.dev@protonmail.ch>
2020-02-02 14:35:29 +0900

    Fix Bashではだと先頭の要素しか出力しないため

--- arch/first/install_system.sh
+++ arch/first/install_system.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 
 
-pacstrap /mnt base base-devel linux 
-pacstrap /mnt plasma konsole dolphin filelight ark gwenview
-pacstrap /mnt zsh git vim neovim words cryfs
+# pacstrap /mnt base base-devel linux
+# pacstrap /mnt plasma konsole dolphin filelight ark gwenview
+# pacstrap /mnt zsh git vim neovim words cryfs
 
-genfstab -U /mnt >> /mnt/etc/fstab
+install_system() {
+  core_pkg=(
+    "base"
+    "base-devel"
+    "linux"
+  )
+  kde_pkg=(
+    "plasma"
+    "konsole"
+    "dolphin"
+    "filelight"
+    "ark"
+    "gwenview"
+    "cryfs"
+  )
+  favorite_pkg=(
+    "zsh"
+    "git"
+    "vim"
+    "neovim"
+    "words"
+  )
 
-exit 0
+  pacstrap /mnt ${core_pkg[@]} ${kde_pkg[@]} ${favorite_pkg[@]}
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
+
+  genfstab -U /mnt >> /mnt/etc/fstab
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
+
+  return 0
+}
+
+
