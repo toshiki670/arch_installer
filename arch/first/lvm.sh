491b0bae9413ab4a5dd8618e9138271e25f97113
Toshiki <toshiki.dev@protonmail.ch>
2020-02-02 13:40:42 +0900

    Change 関数化

--- arch/first/lvm.sh
+++ arch/first/lvm.sh
@@ -2,28 +2,41 @@
 
 path=$1
 
-if [[ $# != 1 ]]; then
-  echo "${0##*/}: Requires 1 argument." >&1
-  exit 16
-fi
+lvm_on_luks(){
+  if [[ $# != 1 ]]; then
+    echo "${0##*/}: Requires 1 argument." >&1
+    exit 16
+  fi
 
-if [[ ! -b ${path} ]]; then
-  echo "${0##*/}: $1 isn't a device file." >&1
-  exit 32
-fi
+  if [[ ! -b ${path} ]]; then
+    echo "${0##*/}: $1 isn't a device file." >&1
+    exit 32
+  fi
 
 
-cryptsetup -v -c serpent-xts-plain64 -s 512 -h sha512 luksFormat ${path}
-cryptsetup luksOpen ${path} decrypted
-pvcreate /dev/mapper/decrypted
-vgcreate system /dev/mapper/decrypted
+  cryptsetup -v -c serpent-xts-plain64 -s 512 -h sha512 luksFormat ${path}
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
 
-lvcreate -l 100%FREE system -n root
+  cryptsetup luksOpen ${path} decrypted
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
 
-# For next version
-# lvcreate -L 50G system -n root
-# lvcreate -l 100%FREE system -n home
+  pvcreate /dev/mapper/decrypted
 
-mkfs.xfs /dev/mapper/system-root
-# mkfs.xfs /dev/mapper/system-home
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
 
+  vgcreate system /dev/mapper/decrypted
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
+
+  lvcreate -l 100%FREE system -n root
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
+
+
+  # For next version
+  # lvcreate -L 50G system -n root
+  # lvcreate -l 100%FREE system -n home
+
+  mkfs.xfs /dev/mapper/system-root
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
+
+  # mkfs.xfs /dev/mapper/system-home
+}
