2c02c95221f748bffef9a8889a522a9603c2d0b5
Toshiki <toshiki.dev@protonmail.ch>
2020-02-03 21:44:22 +0900

    Change クローンするときにバージョンを指定

--- arch/zero.sh
+++ arch/zero.sh
@@ -17,22 +17,20 @@ mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
   echo 'Server = https://jpn.mirror.pkgbuild.com/$repo/os/$arch'
 } > /etc/pacman.d/mirrorlist
 
-# Update database
-pacman -Sy
-if ! pacman -Sy; then
-  echo "${0##*/}: Not updated database." >&1
-  exit 16
-fi
 
-if ! pacman -S git expect; then
+# Update database
+if ! pacman -Sy git expect; then
   echo "${0##*/}: Not installed necesary package." >&1
   exit 32
 fi
 
-if ! git clone https://github.com/toshiki670/linux_installer.git ~/installer; then
+
+REPO_URL="https://github.com/toshiki670/linux_installer.git"
+if ! git clone --branch 0.1.1 --single-branch ${REPO_URL} ~/installer; then
   echo "${0##*/}: Not cloned." >&1
   exit 64
 fi
+
 
 # Time & date
 if ! timedatectl set-ntp true; then
