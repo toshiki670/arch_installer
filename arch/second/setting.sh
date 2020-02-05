f2a5a642e0588e0cb2f898a5c69ef18b0f7b9c52
Toshiki <toshiki.dev@protonmail.ch>
2020-02-05 23:15:04 +0900

    Add hosts #18

--- arch/second/setting.sh
+++ arch/second/setting.sh
@@ -16,3 +16,15 @@ set_locale() {
   return $?
 }
 
+append_hosts() {
+  prefix="Linux"
+  length=8
+  hostname=`cat /dev/urandom | tr -dc "[:alnum:]" | fold -w $length | head -n 1 | sed "s/^/${prefix}-/"`
+
+  {
+    echo '127.0.0.1 localhost'
+    echo '::1       localhost'
+    echo "127.0.1.1 ${hostname}.localdomain  ${hostname}"
+  } >> /etc/hosts
+  return $?
+}
