25fc05ba16c92f892423140ed9336761ab993f76
Toshiki <toshiki.dev@protonmail.ch>
2020-02-05 23:13:57 +0900

    Add timezone #18

--- arch/second/setting.sh
+++ arch/second/setting.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+set_timezone() {
+  ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
+
+  hwclock --systohc --utc
+  return $?
+}
+
