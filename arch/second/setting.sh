7cc41ba213a041b11c873fcc43bea9e81ce023ab
Toshiki <toshiki.dev@protonmail.ch>
2020-02-09 14:22:44 +0900

    Add boot loader #19

--- arch/second/setting.sh
+++ arch/second/setting.sh
@@ -55,3 +55,22 @@ set_initramfs() {
   mkinitcpio -p linux
   return $?
 }
+
+set_boot_loader() {
+  bootctl --path=/boot install
+  result=$?; if [[ $result != 0 ]]; then return $result;fi
+
+  crypto_uuid=`lsblk -Pfip |\
+    grep 'crypto_LUKS' |\
+    sed -r 's/[[:alnum:]]+=/\n&/g' |\
+    awk -F= '$1=="UUID"{print $2}' |\
+    sed 's/"//g'`
+
+  {
+    echo 'title  Arch Linux'
+    echo 'linux /vmlinuz-linux'
+    echo 'initrd /initramfs-linux.img'
+    echo 'options cryptdevice=UUID='${crypto_uuid}':decrypted root=/dev/mapper/system-root nvidia-drm.modeset=1 rw'
+  } >> /boot/loader/entries/arch.conf
+  return $?
+}
