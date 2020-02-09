e9ccf773de49c75750ce3bf4132cf7c69624c420
Toshiki <toshiki.dev@protonmail.ch>
2020-02-09 15:03:26 +0900

    Change version

--- arch/first.sh
+++ arch/first.sh
@@ -46,7 +46,7 @@ main() {
 }
 
 clone_installer(){
-  tag="0.1.1"
+  tag="0.2.0"
   repo_url="https://github.com/toshiki670/linux_installer.git"
   if ! git clone --branch ${tag} --single-branch ${repo_url} /mnt/root/installer; then
     echo "${0##*/}: Not cloned." >&1
