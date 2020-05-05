3e23abab8970292b14d3d1ca93e62d47bf690840
Toshiki <toshiki.dev@protonmail.ch>
2020-05-06 05:58:19 +0900

    Add base command mock 'feature/#45'

--- lib/arch_installer/runner.rb
+++ lib/arch_installer/runner.rb
@@ -3,5 +3,16 @@ require 'arch_installer/version'
 
 module ArchInstaller
   class Runner < Thor
+    package_name 'ArchInstaller'
+
+    desc 'install', %q{Install archlinux system}
+    def install
+      puts 'TODO: install'
+    end
+
+    desc 'setting', %q{Setting after `chroot'}
+    def setting
+      puts 'TODO: setting'
+    end
   end
 end
