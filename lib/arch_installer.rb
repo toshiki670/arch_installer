fe5b9d45395c93aa9949c28f6307c29b348170d9
Toshiki <toshiki.dev@protonmail.ch>
2020-05-09 03:28:02 +0900

    Fix error 'feature/#47'

--- lib/arch_installer.rb
+++ lib/arch_installer.rb
@@ -7,7 +7,7 @@ require 'arch_installer/runner'
 require 'arch_installer/version'
 
 module ArchInstaller
-  class Error < StandardError; end
+  class ArchInstallerError < StandardError; end
 
   class << self
     def install
